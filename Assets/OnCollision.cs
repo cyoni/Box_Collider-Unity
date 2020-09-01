54c03d7d58e6fec5a2ce6b2a461f684f315436ce
Yoni <cyoni10@gmail.com>
2020-09-02 00:23:25 +0300

    first commit

---
+++ Assets/OnCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+        
+
+    private void OnCollisionEnter(Collision collision)
+    {
+   //     Debug.Log("we hit this");
+        if (collision.collider.tag == "Obstacle")
+        {
+           Debug.Log("we hit this");
+            movement.enabled = false;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
